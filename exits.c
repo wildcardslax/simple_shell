e0d7824753e96c1d4fc3837214dea3a31cd1869e
Atlaz <wildcardslax>
2023-09-20 09:48:48 -0700

    simple shell group work

---
+++ exits.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ * _strncpy - Copies a string
+ * @dest: The destination string to copy to
+ * @src: The source string
+ * @n: The number of characters to copy
+ * Return: A pointer to the concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ * _strncat - Concatenates two strings
+ * @dest: The first string
+ * @src: The second string
+ * @n: The maximum number of bytes to use
+ * Return: A pointer to the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: The string to be searched
+ * @c: The character to look for
+ * Return: A pointer to the first occurrence of the character in the string, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
